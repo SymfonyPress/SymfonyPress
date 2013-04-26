df50acd07bfa791961ca9d1fddd0ec6724943bb9
Josiah <josiah@jjs.id.au>
2013-04-26 15:26:25 +1000

    Added a kernel class

---
+++ src/SymfonyPress/Kernel.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace SymfonyPress;
+
+use Symfony\Component\HttpKernel\Kernel as SymfonyKernel;
+
+/**
+ * SymfonyPress Kernel
+ *
+ * @author Josiah <josiah@jjs.id.au>
+ */
+class Kernel extends SymfonyKernel
+{
+}
