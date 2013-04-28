2a1e48eacf4d860b44521ad965b37fcda3a9748c
Josiah <josiah@jjs.id.au>
2013-04-29 07:08:17 +1000

    Renamed kernel class

---
+++ src/SymfonyPress/SymfonyPressKernel.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Copyright (c) 2013 Josiah Truasheim
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @category HttpKernel
+ * @package  SymfonyPress
+ * @author   Josiah <josiah@jjs.id.au>
+ * @license  http://opensource.org/licenses/MIT The MIT License
+ * @link     https://github.com/SymfonyPress/SymfonyPress
+ */
+
+namespace SymfonyPress;
+
+use Symfony\Component\HttpKernel\Kernel as SymfonyKernel;
+
+/**
+ * SymfonyPress Kernel
+ *
+ * @category HttpKernel
+ * @package  SymfonyPress
+ * @author   Josiah <josiah@jjs.id.au>
+ * @license  http://opensource.org/licenses/MIT The MIT License
+ * @link     https://github.com/SymfonyPress/SymfonyPress
+ */
+abstract class SymfonyPressKernel extends SymfonyKernel
+{
+}
